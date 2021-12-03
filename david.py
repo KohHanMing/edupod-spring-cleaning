162ba942b547935983f150ff237c6d7956ec97b5
KohHanMing <kohhanming1997@gmail.com>
2021-12-03 10:44:00 +0800

    Combined code under david.py it now writes to valid and invalid csv

--- david.py
+++ david.py
@@ -1,35 +1,46 @@
 import csv
-import pandas as pd
+import os
 
-filepth = "./.data/source/testinfo.csv"
+#create csv if it doesnt exist.
+def make_csv(file_name):
+    directory = os.path.dirname(file_name)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-df = pd.read_csv(filepth, header=None, usecols=[1])
-df.to_csv("AllDetails.csv", index=False)
-inFile = open("./.data/source/testinfo.csv")
-outFile = open('AllDetails.csv', "w",newline='')
-outFile.truncate()
-inReader =csv.reader(inFile)
-outwriter = csv.writer(outFile)
+filepath = "./.data/source/testinfo.csv"
+valid_filepath = "./.data/target/valid.csv"
+invalid_filepath = "./.data/target/invalid.csv"
+
+make_csv(valid_filepath)
+make_csv(invalid_filepath)
+
+in_file = open(filepath)
+valid_out_file = open(valid_filepath, "w",newline='')
+invalid_out_file = open(invalid_filepath, "w",newline='')
+
+in_reader =csv.reader(in_file)
+valid_out_writer = csv.writer(valid_out_file)
+invalid_out_writer = csv.writer(invalid_out_file)
 
 edge_cases = []
-for row in inReader:
+for row in in_reader:
+    if "-" not in row[1]: #invalid case
+        edge_cases.append(row[1])
+        invalid_out_writer.writerow(row)
+        continue
     row[1] = row[1].upper()
     row[1] = row[1].replace(" ","")
     row[1] = row[1].replace("&AMP;", "AND")
-    if "-" not in row[1]:
-        edge_cases.append(row[1])
-        row[1] = "EDUPOD-" + row[1]
-        outwriter.writerow(row)
-        continue
     position = row[1].rfind("-")
     num = len(row[1]) - position - 1
 
-    if num == 1:
+    #valid cases
+    if num == 1: 
         row[1] = row[1][:position+1] +"00" + row[1][position+1:]
-        outwriter.writerow(row)
+        valid_out_writer.writerow(row)
 
     elif num == 2:
         row[1] = row[1][:position+1] +"0" + row[1][position+1:]
-        outwriter.writerow(row)
+        valid_out_writer.writerow(row)
     else:
-        outwriter.writerow(row)
+        valid_out_writer.writerow(row)
